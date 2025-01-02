4961e73b92086352387921e2ddce72d90f47cff0
BledanIT <bledan@gmail.com>
2025-01-02 14:07:03 +0100

    edit: attribution's tag

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -2,12 +2,12 @@
 
 const Attribution = () => {
   return (
-    <footer>
+    <div>
         <p className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="#">Your Name Here</a>.
         </p>
-    </footer>
+    </div>
   )
 }
 
